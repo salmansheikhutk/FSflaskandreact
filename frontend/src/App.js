0758e779232f9b282c47572bcdfa6e649beb527c
Salman Sheikh <salmansheikhutk@gmail.com>
2024-08-13 12:17:23 -0400

    frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  // you can perform some operations here
+  return (
+    <div>
+      <h1>Welcome to Bizza Platform</h1>
+    </div>
+  );
+}
+
+export default App;
